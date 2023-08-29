3d79d11b919c56c7d96205915e03b9abaf8f899e
Marcin <mkula.poczta@gmail.com>
2023-08-29 16:03:24 +0200

    slight JS amendments: isGameOver logic applied

--- script.js
+++ script.js
@@ -7,47 +7,47 @@ const select = document.querySelector("#score-select")
 
 let score1 = 0
 let score2 = 0
-let playingToValue = select.value
+let isGameOver = false
+let playingToValue = parseInt(select.value)
 
 const resetScores = () => {
 	score1 = 0
 	score2 = 0
 	scoreSpan1.textContent = score1
 	scoreSpan2.textContent = score2
-	scoreSpan1.style.color = "black"
-	scoreSpan2.style.color = "black"
-	playingToValue = select.value
-	playerBtn1.disabled = false
-	playerBtn2.disabled = false
+	scoreSpan1.classList.remove("winner", "loser")
+	scoreSpan2.classList.remove("winner", "loser")
+	playingToValue = parseInt(select.value)
+    isGameOver = false
 }
 
 select.addEventListener("change", () => {
 	resetScores()
-	playingToValue = select.value
 })
 
 playerBtn1.addEventListener("click", () => {
-	score1++
-	if (score1 < playingToValue) {
-		scoreSpan1.textContent = score1
-	} else if (score1 == playingToValue) {
-		scoreSpan1.textContent = score1
-		scoreSpan1.style.color = "green"
-		scoreSpan2.style.color = "red"
-		playerBtn2.disabled = true
+	if (!isGameOver) {
+		score1++
+		if (score1 === playingToValue) {
+			isGameOver = true
+			scoreSpan1.classList.add("winner")
+			scoreSpan2.classList.add("loser")
+		}
 	}
+	scoreSpan1.textContent = score1
 })
 
 playerBtn2.addEventListener("click", () => {
-	score2++
-	if (score2 < playingToValue) {
-		scoreSpan2.textContent = score2
-	} else if (score2 == playingToValue) {
-		scoreSpan2.textContent = score2
-		scoreSpan2.style.color = "green"
-		scoreSpan1.style.color = "red"
-		playerBtn1.disabled = true
+	if (!isGameOver) {
+		score2++
+		if (score2 === playingToValue) {
+			isGameOver = true
+			scoreSpan2.classList.add("winner")
+			scoreSpan1.classList.add("loser")
+		}
 	}
+	scoreSpan2.textContent = score2
 })
+
 
 resetBtn.addEventListener("click", resetScores)
